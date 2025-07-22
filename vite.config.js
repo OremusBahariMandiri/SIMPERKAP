16e765c19e76999e79e7654e630b0e2930d11592
Jamas108 <oremus.itdept@gmail.com>
2025-07-22 09:48:22 +0700

    env

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,23 @@ export default defineConfig({
         laravel({
             input: [
                 'resources/sass/app.scss',
-                
+
                 'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
-    // server: {
-    //     host: '0.0.0.0',
-    //     hmr: {
-    //         host:'192.168.0.39'
-    //     },
-    //     cors: {
-    //         origin: ['http://192.168.0.39:8080', 'http://127.0.0.1:8000'],
-    //         methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    //         credentials: true
-    //     }
-    // },
+    server: {
+        host: '0.0.0.0',
+        hmr: {
+            host:'192.168.0.39'
+        },
+        cors: {
+            // origin: ['http://192.168.0.39:8081', 'http://127.0.0.1:8000'], // Server JAMAS
+            origin: ['http://192.168.1.24:8081', 'http://127.0.0.1:8000'], // SERVER OREMUS
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            credentials: true
+        }
+    },
+
 });
